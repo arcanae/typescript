9532d94d0ce97679a6047a03dfe79a6047d1c45a
Arcanae <justin.courdesse@gmail.com>
2017-09-05 11:24:51 +0200

    first commit

---
+++ dist/first.js
@@ -0,0 +1,10 @@
+var myvar = "bloup";
+var varnum = 21.21;
+var varstr = 'banane';
+for (var i = 1; i < 10; i++) {
+    if (varnum <= 30) {
+        varnum += 3;
+    }
+}
+var tableString = ["yo", "slt", "banane", "kiwi"];
+//# sourceMappingURL=first.js.map
---
+++ first.ts
@@ -0,0 +1,14 @@
+let myvar = "bloup";
+
+let varnum:number = 21.21;
+
+let varstr:string = 'banane';
+
+for (let i = 1; i < 10 ; i++) {
+	if ( varnum <= 30) {
+		varnum += 3;
+	}
+}
+
+let tableString:string[] = ["yo","slt","banane","kiwi"];
+
